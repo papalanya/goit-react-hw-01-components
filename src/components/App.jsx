29f4ba061d9ff244513cd0332d94a8319173ea05
Illia Pavlenko <illiampavlenko@gmail.com>
2023-01-13 04:08:20 +0200

    add task 2, statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import { Profile } from './Profile/profile';
 import user from 'data/user.json';
 
+import { Statistics } from './Statistics/statistics';
+import data from 'data/data.json';
+
 
 
 export const App = () => {
   return (
     <div>
+      
       <Profile 
         username={user.username}
         tag={user.tag}
@@ -13,11 +17,14 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
        /> 
-      {/* <Statistics
-            title="Upload stats" stats={statistics}
-          />
-      <FriendList friends={friends} />
+      
+      <Statistics
+            title="Upload stats" stats={data}
+      />
+      
+      {/* <FriendList friends={friends} />
       <TransactionHistory items={transactions} /> */}
+
     </div>
 
   );
---
+++ src/components/Statistics/statistics.jsx
@@ -0,0 +1,35 @@
+import css from './statistics.module.css'
+import PropTypes from 'prop-types';
+import randomColor from './randomColor';
+
+export const Statistics = ({ title, stats }) => {
+    return (
+      <section className={css.statistics}>
+        {title && <h2 className={css.title}>{title}</h2>}
+  
+        <ul className={css.list}>
+          {stats.map(({id, label, percentage}) => {
+            return (
+                <li className={css.item}
+                    key={id}
+                    style={{ backgroundColor: randomColor() }} >
+                <span className={css.label}>{label}</span>
+                <span className={css.percentage}>{percentage}%</span>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    );
+  };
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            label: PropTypes.string.isRequired,
+            percentage: PropTypes.number.isRequired,
+        })
+    ),
+};
