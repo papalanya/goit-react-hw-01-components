09d17c29547dad346369c828d16d272c70d98c12
Illia Pavlenko <illiampavlenko@gmail.com>
2023-01-13 02:55:47 +0200

    add task 1, profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { Profile } from './Profile/profile';
+import user from 'data/user.json';
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile 
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+       /> 
+      {/* <Statistics
+            title="Upload stats" stats={statistics}
+          />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} /> */}
     </div>
+
   );
 };
